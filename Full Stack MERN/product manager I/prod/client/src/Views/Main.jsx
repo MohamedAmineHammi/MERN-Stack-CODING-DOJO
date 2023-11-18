926edba311a553f0106bfd7d532160d40ae5c0f6
MohamedAmineHammi <eminehammi@gmail.com>
2023-11-18 09:58:03 +0100

    Views

---
+++ Full Stack MERN/product manager I/prod/client/src/Views/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Dashbord } from '../components/Dashbord'
+import { Form } from '../components/Form'
+
+const Main = () => {
+  return (
+    <div>
+        <Form />
+    </div>
+  )
+}
+
+export default Main
