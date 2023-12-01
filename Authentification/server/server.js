cc4bd971186765f1014285c7cc17703d5a8b2ad6
MohamedAmineHammi <eminehammi@gmail.com>
2023-12-01 23:40:21 +0100

    Authentification

---
+++ Authentification/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+//Configure dotenv files above using any other library and files
+dotenv.config({ path: './config/config.env' });
+//Creating an app from express
+const app = express();
+const cors = require("cors");
+const db_name = 'users';
+//Middleware
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }), express.json(), express.urlencoded({ extended: true }));
+
+//listening to the server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening at ${process.env.PORT}`);
+})
+
+// Authentication middleware
+require('./config/jwt.config')
+// database connection
+require("./config/mongoose.config")(db_name);
+require('./routes/user.routes')(app);
